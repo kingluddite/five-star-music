6f20ac718daee33f6966837dd7966ff1fcf4c2b9
Kingluddite <howley.phil@gmail.com>
2018-09-22 00:49:02 -0700

    Update css

--- client/src/index.js
+++ client/src/index.js
@@ -32,8 +32,8 @@ import Profile from './components/Profile/Profile';
 
 // Apollo client
 const client = new ApolloClient({
-  // uri: 'http://localhost:4444/graphql',
-  uri: 'https://fivestarsongs.herokuapp.com/graphql',
+  uri: 'http://localhost:4444/graphql',
+  // uri: 'https://fivestarsongs.herokuapp.com/graphql',
   fetchOptions: {
     credentials: 'include',
   },
